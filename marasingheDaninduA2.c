be0ee119d41810736bc78f988f80bdf4cb0e08d3
bmarasi <danindum@outlook.com>
2020-11-08 22:51:08 -0500

    Created all function stubs

--- marasingheDaninduA2.c
+++ marasingheDaninduA2.c
@@ -13,3 +13,47 @@ and all material that I have used, whether directly quoted or
 paraphrased. Furthermore, I certify that this assignment was prepared by
 me specifically for this course.
  ********************************************************/
+
+void createInitialBoard(char[N][N]){
+    printf("FIXME: Create initial board\n");
+}
+void readUserMove(int *, int *, int *){
+    printf("FIXME: Read User Move\n");
+}
+void printCurrentBoard(char[N][N]){
+    printf("FIXME: Print current board\n");
+}
+void computerPlaysRandom(int * , int *){
+    printf("FIXME: Computer plays random\n");
+}
+int computerPlaysToWin(char [N][N], int * , int * ){
+    printf("FIXME: Computer plays to win\n");
+    return 1;
+}
+int getComputerMove(char board[N][N], int *, int *, int, int){
+    printf("FIXME: Get computer move\n");
+    return 1;
+}
+int gameWon(char [N][N], char){
+    printf("FIXME: Games won\n");
+    return 1;
+}
+int isInputValid(int, int, int){
+    printf("FIXME: Is input valid\n");
+    return 1;
+}
+void all_sums(char board[N][N], int [N], int [N], int *, int *){
+    printf("FIXME: all sums\n");
+}
+int memberOf(int value, int someArray[N]){
+    printf("FIXME: member of\n");
+    return 1;
+}
+int isBoardFull(char board[N][N]){
+    printf("FIXME: is board full\n");
+    return 1;
+}
+int computerPlaysToBlock(char [N][N], int * , int * ){
+    printf("FIXME: computer plays to block\n");
+    return 1;
+}
