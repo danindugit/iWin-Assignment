d52857aadb062cbb45534431b3be27baa9033d52
bmarasi <danindum@outlook.com>
2020-11-09 21:19:00 -0500

    Updated done functions

--- givenA2.h
+++ givenA2.h
@@ -11,7 +11,7 @@ void computerPlaysRandom(int * , int *);
 int computerPlaysToWin(char [N][N], int * , int * );
 int getComputerMove(char board[N][N], int *, int *, int, int);
 int gameWon(char [N][N], char);
-int isInputValid(int, int, int);
+int isInputValid(int, int, int);  //done
 void all_sums(char board[N][N], int [N], int [N], int *, int *);
 int memberOf(int value, int someArray[N]);
 int isBoardFull(char board[N][N]);  //done
